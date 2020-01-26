8045c74bf0da254ddf7dbb1ebc49b468e8d461ca
swaroopmalladidev <swaroop.malladi@gmail.com>
2020-01-27 02:15:59 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import Header from "./Header"
 
 class App extends Component {
     render() {
+
         return ( 
         <div>
-            Phone Directory
-        <div className="header"> 
-            Phone Directory 
-        </div>
-        <button> Add </button>
-        <div>
-            <span>Name</span><br/>
-            <span>Phone</span>
-        </div>
+            <Header/>
+            <button> Add </button>
+            <div>
+                <span>Name</span><br/>
+                <span>Phone</span>
+            </div>
         </div>
         );
     }
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Header = function(){
+    return(
+        <div className="header"> 
+                Phone Directory 
+            </div>
+    )
+}
+
+// class Header extends ReactComponent {
+//     render(){
+//         return(
+//             <div className = "header">
+//                 Phone Directory
+//             </div>
+//         )
+//     }
+// }
+
+export default Header;
